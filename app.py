58a1e3a031b34472da02addf371e73027e2d5220
Ahmed Ibrahim <ahmed.ibrahimsalem2@gmail.com>
2025-04-07 12:55:30 +0200

    Initial deployment

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ def generate_team():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
